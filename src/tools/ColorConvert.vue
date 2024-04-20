0491c8d1481db2bef8298bd716f0a1b404f75238
uncenter <47499684+uncenter@users.noreply.github.com>
2024-04-20 15:52:49 -0400

    feat(colorconvert): switch to tinycolor, add randomization

--- src/tools/ColorConvert.vue
+++ src/tools/ColorConvert.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
 import { set, get } from '@vueuse/core';
-import { ColorTranslator } from 'colortranslator';
+import { TinyColor, random } from '@ctrl/tinycolor';
 
 const input = useStorage('colorconvert__color', '');
-const color: Ref<ColorTranslator | undefined> = ref();
+const color: Ref<TinyColor> = ref(new TinyColor());
 
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Shuffle } from 'lucide-vue-next';
 
 function update() {
-	try {
-		set(color, new ColorTranslator(get(input)));
-	} catch {
-		try {
-			set(color, new ColorTranslator('#' + get(input)));
-		} catch {
-			set(color, undefined);
-		}
-	}
+	set(color, new TinyColor(get(input)));
 }
 
 watch(input, update);
@@ -25,35 +19,69 @@ watch(input, update);
 <template>
 	<section class="flex flex-row gap-2 items-center">
 		<Input v-model="input" />
+		<Button size="icon" @click="input = random().toHexString()"
+			><Shuffle class="size-4"
+		/></Button>
+	</section>
+	<div
+		v-if="color.isValid"
+		class="border rounded-md py-2 px-4 flex flex-row justify-between items-center"
+	>
+		<dl class="w-2/3 max-w-96">
+			<div>
+				<dt>Hex</dt>
+				<dd>
+					<code>{{ color.toHexString() }}</code>
+				</dd>
+			</div>
+			<div>
+				<dt>HSL</dt>
+				<dd>
+					<code>{{ color.toHslString() }}</code>
+				</dd>
+			</div>
+			<div>
+				<dt>RGB</dt>
+				<dd>
+					<code>{{ color.toRgbString() }}</code>
+				</dd>
+			</div>
+			<div>
+				<dt>CMYK</dt>
+				<dd>
+					<code>{{ color.toCmykString() }}</code>
+				</dd>
+			</div>
+			<div>
+				<dt>HSV</dt>
+				<dd>
+					<code>{{ color.toHsvString() }}</code>
+				</dd>
+			</div>
+			<div>
+				<dt>Name</dt>
+				<dd>
+					<code>{{ color.toName() || 'N/A' }}</code>
+				</dd>
+			</div>
+		</dl>
 		<div
-			class="size-10 rounded-full border-solid border-2"
+			class="size-36 rounded-full border-solid border-2"
 			:style="{
-				'background-color': color?.HEX,
+				'background-color': color?.toHexString(),
 			}"
 		></div>
-	</section>
-	<div v-if="color instanceof ColorTranslator">
-		<table>
-			<tbody>
-				<tr>
-					<th scope="row">Hex</th>
-					<td>
-						<code>{{ color.HEX }}</code>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">HSL</th>
-					<td>
-						<code>{{ color.HSL }}</code>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">RGB</th>
-					<td>
-						<code>{{ color.RGB }}</code>
-					</td>
-				</tr>
-			</tbody>
-		</table>
 	</div>
 </template>
+
+<style lang="scss">
+dl div {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+
+	dt {
+		font-weight: 500;
+	}
+}
+</style>
